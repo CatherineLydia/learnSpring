23f5e28255686c2abae483ae3a5cadf5f37c4952
Catherine Lydia Philomin Raj <catherinepr@authenticid.com>
2022-04-21 10:18:12 -0400

    New Spring project - Tight coupling

---
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
@@ -0,0 +1,11 @@
+package com.in28minutes.spring.basics.springin10steps;
+
+public class BinarySearchImpl {
+
+    public int binarySearch(int[] numbers,int numberToSearch){
+        return 3;
+    }
+    //Sorting an array
+    //Search the array
+    //Return the result
+}
---
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.spring.basics.springin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn10StepsApplication {
+
+	public static void main(String[] args) {
+
+		BinarySearchImpl binarySearch = new BinarySearchImpl();
+		int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
+		System.out.println(result);
+		SpringApplication.run(SpringIn10StepsApplication.class, args);
+	}
+
+}
