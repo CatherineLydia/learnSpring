21a2f7cb1ab0884911197a1c9e08ee02da49c884
Catherine Lydia Philomin Raj <catherinepr@authenticid.com>
2022-04-21 11:22:52 -0400

    Step 4 :Using Spring to manage dependencies

--- src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
@@ -1,7 +1,12 @@
 package com.in28minutes.spring.basics.springin10steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BinarySearchImpl {
 
+    @Autowired
     private SortAlgorithm sortAlgorithm;
 
     public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
--- src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
@@ -2,17 +2,31 @@ package com.in28minutes.spring.basics.springin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringIn10StepsApplication {
 
+	//What are the beans?
+	//What are the dependencies of a bean?
+	//Where to search for beans?
+
 	public static void main(String[] args) {
 
 		//Loose coupling - can BubbleSort or QuickSort
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+
+
+		//Application Context
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsApplication.class, args);
+
+		//get the bean from applicationContext
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+
 		int result = binarySearch.binarySearch(new int[]{12, 4, 6}, 3);
+
 		System.out.println(result);
-		SpringApplication.run(SpringIn10StepsApplication.class, args);
+
 	}
 
 }
