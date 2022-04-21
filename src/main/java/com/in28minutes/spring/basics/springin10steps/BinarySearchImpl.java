c33f5b7b26873518c5d1b315c8af6b455c7b6511
Catherine Lydia Philomin Raj <catherinepr@authenticid.com>
2022-04-21 16:55:24 -0400

    Step 13 :Autowiring - @Qualifier

--- src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
@@ -1,21 +1,23 @@
 package com.in28minutes.spring.basics.springin10steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
     @Autowired
-    private SortAlgorithm bubbleSortAlgorithm;
+    @Qualifier("quick")
+    private SortAlgorithm sortAlgorithm;
 
     //no constructor- no setter injection
 
 
     public int binarySearch(int[] numbers, int numberToSearch){
 
-        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
-        System.out.println(bubbleSortAlgorithm);
+        int[] sortedNumbers = sortAlgorithm.sort(numbers);
+        System.out.println(sortAlgorithm);
         //Search the array
 
         //Return the result
