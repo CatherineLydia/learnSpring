a01c260f0b7ba9a6d051cab878513d81cfe91a8a
Catherine Lydia Philomin Raj <catherinepr@authenticid.com>
2022-04-21 10:30:53 -0400

    Step 2 :Understanding Tight coupling using BinarySearch Algorithm

--- src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/BinarySearchImpl.java
@@ -2,10 +2,19 @@ package com.in28minutes.spring.basics.springin10steps;
 
 public class BinarySearchImpl {
 
+
+
     public int binarySearch(int[] numbers,int numberToSearch){
+        //Sorting an array- BubbleSort
+        BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+
+//        QuickSortAlgorithm quickSortAlgorithm =new QuickSortAlgorithm();
+
+        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+        //Search the array
+
+        //Return the result
         return 3;
     }
-    //Sorting an array
-    //Search the array
-    //Return the result
+
 }
---
+++ src/main/java/com/in28minutes/spring/basics/springin10steps/BubbleSortAlgorithm.java
@@ -0,0 +1,10 @@
+package com.in28minutes.spring.basics.springin10steps;
+
+public class BubbleSortAlgorithm {
+
+    public int[] sort(int[] numbers){
+        //Logic for Bubble Sort
+        return numbers;
+    }
+
+}
